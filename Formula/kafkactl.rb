94459d42180eb8a227dfbf223a63f8f7e9edb79c
goreleaserbot <goreleaser@device-insight.com>
2020-02-28 11:44:53 +0100

    Brew formula update for kafkactl version 1.6.0

--- Formula/kafkactl.rb
+++ Formula/kafkactl.rb
@@ -10,11 +10,11 @@ In addition kafkactl supports auto-completion for its commands as well as topic 
 
   if OS.mac?
     url "https://github.com/deviceinsight/kafkactl/releases/download/1.6.0/kafkactl_1.6.0_Darwin_x86_64.tar.gz"
-    sha256 "e05ac360a1b588cbf21ea6a530569f3fb64a1ab20423f8a4b381ea199b88693b"
+    sha256 "7be4500f20743868e8f877301c0468f7f9244432ef90763a66464322168e4da5"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/deviceinsight/kafkactl/releases/download/1.6.0/kafkactl_1.6.0_Linux_x86_64.tar.gz"
-      sha256 "f6365c4df4462ab27b6cc3c51a2faf770830d22e197058597e67e3895a2c7d93"
+      sha256 "4c9c75b841336198c4d20b012102b66465c9eff50183c6b111f42442d53099c3"
     end
   end
 
