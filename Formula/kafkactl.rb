56099c9a3dd927f49a0d22a25c78b2ede24a6c2f
goreleaserbot <goreleaser@device-insight.com>
2021-08-06 12:01:14 +0200

    Brew formula update for kafkactl version v1.20.0

--- Formula/kafkactl.rb
+++ Formula/kafkactl.rb
@@ -8,28 +8,28 @@ Multiple kafka brokers can be configured in a config file and the active broker 
 In addition kafkactl supports auto-completion for its commands as well as topic names.
 "
   homepage "https://www.device-insight.com/"
-  version "1.19.0"
+  version "1.20.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.19.0/kafkactl_1.19.0_darwin_amd64.tar.gz"
-      sha256 "6f92ff5d515e95d96d015d9996bb9716cf7e0061aa22f96feea238889609520f"
+      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.20.0/kafkactl_1.20.0_darwin_amd64.tar.gz"
+      sha256 "5efe707bfb4e7825297f1ee9226a76aabee11567311b2300a61d626032593281"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.19.0/kafkactl_1.19.0_darwin_arm64.tar.gz"
-      sha256 "3e31b73eef576fe4ace198fad3d39c78fe341b677682b2bc3feefb23f0a62c1b"
+      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.20.0/kafkactl_1.20.0_darwin_arm64.tar.gz"
+      sha256 "b1edbf8e24f8d27256daa4a8ac10af4c5a4e2aabf70acb1572881a14f4aa9ff8"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.19.0/kafkactl_1.19.0_linux_amd64.tar.gz"
-      sha256 "a92006a09f4c3514c914d16dbe15d5b52336515709d337f83da8ff711cd24a80"
+      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.20.0/kafkactl_1.20.0_linux_amd64.tar.gz"
+      sha256 "4af8fb4dfde524be6ba0d5e168b5ca2d435baeb538a63a4cc78cee00d9f27074"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.19.0/kafkactl_1.19.0_linux_arm64.tar.gz"
-      sha256 "00c38232dc63746036eb83ac5dd63993c5a06a7a0cda26f7b670dea304fad889"
+      url "https://github.com/deviceinsight/kafkactl/releases/download/v1.20.0/kafkactl_1.20.0_linux_arm64.tar.gz"
+      sha256 "02689b9b8e0c8703d66c962caa25a3836fb074ee12bba09390efab3bf774a312"
     end
   end
 
